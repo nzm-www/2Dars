a45b751638d448d738cecb70b9d8fb1652871f5a
nzm-www <nizomiddinabdusattorov50@gmail.com>
2024-04-28 00:00:19 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,139 @@
+//1-masala
+
+// let num1 = +prompt("a");
+// let num2 = +prompt("b");
+// function sum(a, b) {
+//   let resul = a + b;
+//   return resul;
+// }
+// console.log(sum(num1, num2));
+
+//2masala
+
+// let num1 = +prompt("a");
+// function sum(a) {
+//   let resul = a * 60;
+//   return resul;
+// }
+// console.log(sum(num1));
+
+//3-masala
+
+// let num1 = +prompt("a");
+// function sum(a) {
+//   let resul = a + 1;
+//   return resul;
+// }
+// console.log(sum(num1));
+
+//4-masala
+
+// let num1 = +prompt("asos");
+// let num2 = +prompt("balandlik");
+
+// function uchburchakYuzi(asos, balandlik) {
+//   let natija = (asos * balandlik) / 2;
+//   return natija;
+// }
+
+// console.log(uchburchakYuzi(num1, num2));
+
+//5-masala
+
+// let num = +prompt("age");
+// function ageToDays(age) {
+//   let natija = num * 365;
+//   return natija;
+// }
+// console.log(ageToDays(num));
+
+//6-masala
+
+// let num = +prompt("son");
+// function kub(son) {
+//   let result = num * num * num;
+//   return result;
+// }
+// console.log(kub(num));
+
+//7-masala
+
+// let num1 = +prompt("son1");
+// let num2 = +prompt("son2");
+// let num3 = +prompt("son3");
+
+// function firstElement(son1, son2, son3) {
+//   return num1;
+// }
+
+// console.log(firstElement(num1));
+
+//8-masala
+
+// let num1 = +prompt("son1");
+// let num2 = +prompt("son2");
+
+// function power(son1, son2) {
+//   let result = son1 * son2;
+//   return result;
+// }
+
+// console.log(power(num1, num2));
+
+//9-masala
+
+// let son = +prompt("num");
+
+// function hourToSekunds(num) {
+//   let result = num * 3600;
+//   return result;
+// }
+
+// console.log(hourToSekunds(son));
+
+//10-masala
+
+// let num1 = +prompt("son1");
+// let num2 = +prompt("son2");
+
+// function uchinchiTomon(son1, son2) {
+//   let result = num1 + num2 - 1;
+//   return result;
+// }
+
+// console.log(uchinchiTomon(num1, num2));
+
+//11-masala
+
+// let num1 = +prompt("son1");
+// let num2 = +prompt("son2");
+
+// function uchinchiTomon(son1, son2) {
+//   let result = num1 % num2;
+//   return result;
+// }
+
+// console.log(uchinchiTomon(num1, num2));
+
+//12-masala
+
+// let num1 = +prompt("boyi");
+// let num2 = +prompt("eni");
+
+// function tortburchakningyuzi(boyi, eni) {
+//   let result = num1 * num2;
+//   return result;
+// }
+
+// console.log(tortburchakningyuzi(num1, num2));
+
+//13-masala
+
+let soz = +prompt("matn");
+
+function stringkoshish(matn) {
+  let result = "something" + matn;
+  return result;
+}
+
+console.log(stringkoshish(soz));
